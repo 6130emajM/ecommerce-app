9cc9052716f869d1157515b9d767bf4809268bbe
6130emajM <15ehtkradliveegasgnikramylix34@gmail.com>
2026-06-30 17:27:26 -0400

    Remove duplicate jest config files from src/pages

--- src/pages/jest.config.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  setupFilesAfterEach: [],
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  moduleNameMapper: {
-    '\\.(css|less|scss)$': 'identity-obj-proxy',
-  },
-};
